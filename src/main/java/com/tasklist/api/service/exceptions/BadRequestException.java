dd9f969526909b57c6247787eed82bf9e1e9f231
Weisberg Barros <weisbergbsf@gmail.com>
2020-05-05 20:58:23 -0300

    Created ContollerAdvice for exceptions

---
+++ src/main/java/com/tasklist/api/service/exceptions/BadRequestException.java
@@ -0,0 +1,15 @@
+package com.tasklist.api.service.exceptions;
+
+public class BadRequestException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public BadRequestException(String msg) {
+		super(msg);
+	}
+
+	public BadRequestException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+
+}
